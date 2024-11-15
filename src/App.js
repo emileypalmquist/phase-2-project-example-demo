58db3aba7959be6fb64a252ccc9fc6668c5208f2
Emiley Palmquist <emiley.palmquist@gmail.com>
2024-11-15 08:37:42 -0800

    ignore env add fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+
+const BASE_URL = process.env.REACT_APP_JSON_SERVER
 
 function App() {
+
+  useEffect(() => {
+    fetch(BASE_URL + "/example")
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
